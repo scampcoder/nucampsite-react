171189a1580cbe218fd59a3c1d6e4d7500014b88
Victoria Kee <victoriakee95@gmail.com>
2021-11-13 12:47:55 -0600

    create renderComments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,25 +1,44 @@
 import React, {Component} from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardBody, CardText, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
     renderCampsite(campsite) {
         return (
             <div className="col-md-5 m-1">
-                <Card onClick={() => this.onCampsiteSelect(campsite)}>
-                    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                    <CardImgOverlay>
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
                         <CardTitle>{campsite.name}</CardTitle>
-                    </CardImgOverlay>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
                 </Card>
             </div>
         )
     }
     
+    renderComments(comments) {
+        if(comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    { comments.map(comment =>
+                        <p>
+                            { comment.text } <br />
+                            --{ comment.author }, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                        </p>
+                    ) }
+                </div>
+            )
+        }
+        return <div></div>
+    }
+
     render() {
         if(this.props.campsite) {
             return (
                 <div className="row">
                     { this.renderCampsite(this.props.campsite) }
+                    { this.renderComments(this.props.campsite.comments) }
                 </div>
             )
         }
