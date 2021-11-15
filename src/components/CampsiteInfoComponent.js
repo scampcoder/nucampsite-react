400c404453bd97290e8956cfe66adc7a8f773cc9
Victoria Kee <victoriakee95@gmail.com>
2021-11-15 11:34:21 -0600

    Fix styling error

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -36,9 +36,11 @@ class CampsiteInfo extends Component {
     render() {
         if(this.props.campsite) {
             return (
-                <div className="row">
+                <div className="container">
+                    <div className="row">
                     { this.renderCampsite(this.props.campsite) }
                     { this.renderComments(this.props.campsite.comments) }
+                </div>
                 </div>
             )
         }
