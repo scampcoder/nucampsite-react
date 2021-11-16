3497ac5cd3b3c609cce9bf515db9caa0715d6976
Victoria Kee <victoriakee95@gmail.com>
2021-11-16 16:14:27 -0600

    Add homecomp and update main to use it

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites.js';
 
 class Main extends Component {
@@ -14,16 +15,23 @@ class Main extends Component {
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
+
 
     render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
         return (
             <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)} />
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                <Route path='/home' component={HomePage} />
+                    <Directory campsites={this.state.campsites} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites } />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
