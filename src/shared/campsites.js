58240d50059b9a550f12d77d63201aefb0f9aa84
Victoria Kee <victoriakee95@gmail.com>
2021-11-17 10:52:49 -0600

    Add new pages with code from exercise

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -1,178 +1,34 @@
 export const CAMPSITES = [
     {
         id: 0,
-        name: 'React Lake Campground',
-        image: 'assets/images/react-lake.jpg',
+        name: "React Lake Campground",
+        image: "/assets/images/react-lake.jpg",
         elevation: 1233,
-        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "What a magnificent view!",
-                author: "Tinus Lorvaldes",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "A wonderful place to reconnect with nature.",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The stars at night were magnificent!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        featured: false,
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
     },
     {
         id: 1,
-        name:'Chrome River Campground ',
-        image: 'assets/images/chrome-river.jpg',
+        name:"Chrome River Campground ",
+        image: "/assets/images/chrome-river.jpg",
         elevation: 877,
-        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "Goodness gracious so many deer!",
-                author: "Lori Green",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "Couldn't get any sleep with all the owls.",
-                author: "Bree Caldwell",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "What a phenomenal time. Clear water, bright skies, amazing!",
-                author: "Nick Rooney",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "Bring a few beers and you'll have a swell time.",
-                author: "Josh Treddie",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "I couldn't belive there were so many squirrels!",
-                author: "Camille Lane",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        featured: false,
+        description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
     },
     {
         id: 2,
-        name:'Breadcrumb Trail Campground',
-        image: 'assets/images/breadcrumb-trail.jpg',
+        name:"Breadcrumb Trail Campground",
+        image: "/assets/images/breadcrumb-trail.jpg",
         elevation: 2901,
-        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "Sites like these make me wanna quit my job and move to the forest.",
-                author: "Sammy Delo",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "Bring your swimsuit, there is a lake just a short hike from here with crystal clear water.",
-                author: "Sara Trejo",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "PUT UP YOUR FOOD! Bears in the area scraped up our cooler pretty bad.",
-                author: "Lela Yentz",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "It gets pretty cold here at night, but the day is magnificent.",
-                author: "Teresa Plattes",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "The stars at night were magnificent!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        featured: false,
+        description: "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground."
     },
     {
         id: 3,
-        name:'Redux Woods Campground',
-        image: 'assets/images/redux-woods.jpg',
+        name:"Redux Woods Campground",
+        image: "/assets/images/redux-woods.jpg",
         elevation: 42,
-        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
-        comments: [
-            {
-                id: 0,
-                rating: 5, 
-                text: "Too many four-wheelers.",
-                author: "Bob Vance",
-                date: "2018-10-25T16:30Z"
-            },
-            {
-                id: 1,
-                rating: 4, 
-                text: "Great place for a company retreat.",
-                author: "Michael Scott",
-                date: "2017-06-17T03:33Z"
-            },
-            {
-                id: 2,
-                rating: 5, 
-                text: "Amazing spot to bird watch.",
-                author: "Ged Hawk",
-                date: "2019-02-18T18:12Z"
-            },
-            {
-                id: 3,
-                rating: 5, 
-                text: "Very dog friendly!",
-                author: "Dunnie Clov",
-                date: "2019-08-04T20:11Z"
-            },
-            {
-                id: 4,
-                rating: 4, 
-                text: "Very secluded, pack light.",
-                author: "Anna Karenina",
-                date: "2018-07-23T19:44Z"
-            }
-        ]
+        featured: true,
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
     }
 ];
