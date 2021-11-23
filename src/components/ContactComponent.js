a34ea5e6c4297e46cac70ce5c77cae85f050918b
Victoria Kee <victoriakee95@gmail.com>
2021-11-23 15:34:45 -0600

    create input handler method

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { valueToNode } from '@babel/types';
 
 class Contact extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {
             firstName: '',
             lastName: '',
@@ -16,6 +17,17 @@ class Contact extends Component {
             feedback: ''
         };
     }
+
+    handleInputChange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+    
     render() {
         return (
             <div className="container">
