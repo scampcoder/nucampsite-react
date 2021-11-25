a1a584aee5986d9401317616275942ec4a3aea0f
Victoria Kee <victoriakee95@gmail.com>
2021-11-25 17:49:34 -0600

    import from redux-form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
 
 class Contact extends Component {
     constructor(props) {
