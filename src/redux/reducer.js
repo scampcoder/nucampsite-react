79db918d64e679e145387c0b3e8c538b53695c00
Victoria Kee <victoriakee95@gmail.com>
2021-11-25 11:56:00 -0600

    set up initial state and reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -3,3 +3,13 @@ import { PARTNERS } from "../shared/partners";
 import { PROMOTIONS } from "../shared/promotions";
 import { COMMENTS } from "../shared/comments";
 
+export const initialState = {
+    campsites: CAMPSITES,
+    comments: COMMENTS,
+    partners: PARTNERS,
+    promotions: PROMOTIONS
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+}
