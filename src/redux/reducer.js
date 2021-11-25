e9ec11e2de465a09e2fe10fa1daccf9baf2617a3
Victoria Kee <victoriakee95@gmail.com>
2021-11-25 11:54:06 -0600

    import state data to reducer file

---
+++ src/redux/reducer.js
@@ -0,0 +1,5 @@
+import { CAMPSITES } from "../shared/campsites";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
+import { COMMENTS } from "../shared/comments";
+
