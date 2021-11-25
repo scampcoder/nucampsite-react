d774f9671894f6480b66bdd36dbbbaab8d73abfd
Victoria Kee <victoriakee95@gmail.com>
2021-11-25 17:51:21 -0600

    update handleSubmit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -22,7 +22,6 @@ class Contact extends Component {
                 email: false
             }
         };
-        this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -62,12 +61,6 @@ class Contact extends Component {
         return errors;
     }
 
-    handleBlur = (field) => () => {
-        this.setState({
-            touched: {...this.state.touched, [field]: true}
-        });
-    }
-
     handleInputChange(event) {
         const target = event.target;
         const name = target.name;
@@ -78,10 +71,9 @@ class Contact extends Component {
         });
     }
 
-    handleSubmit(event) {
-        console.log('Current state is: ' + JSON.stringify(this.state));
-        alert('Current state is: ' + JSON.stringify(this.state));
-        event.preventDefault();
+    handleSubmit(values) {
+        console.log('Current state is: ' + JSON.stringify(values));
+        alert('Current state is: ' + JSON.stringify(values));
     }
 
     render() {
