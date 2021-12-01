2c2cee4547775be09af5775ae7631247edcef047
Victoria Kee <victoriakee95@gmail.com>
2021-12-01 09:59:22 -0600

    update campsite props

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -27,11 +27,17 @@ const mapDispatchToProps = {
 
 class Main extends Component {
   
+    componentDidMount() {
+        this.props.fetchCampsites();
+    }
+
     render() {
         const HomePage = () => {
             return (
                 <Home 
-                    campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                    campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                    campsitesLoading={this.props.campsites.isLoading}
+                    campsitesErrMess={this.props.campsites.errMess}
                     partner={this.props.partners.filter(partner => partner.featured)[0]}
                     promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
                 />
@@ -41,7 +47,9 @@ class Main extends Component {
         const CampsiteWithId = ({match}) => {
             return(
                 <CampsiteInfo 
-                    campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    campsitesLoading={this.props.campsites.isLoading}
+                    campsitesErrMess={this.props.campsites.errMess}
                     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}  
                     addComment={this.props.addComment}
                 />
