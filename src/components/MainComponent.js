0f38f39347bf047ae08d3e3584ed4332a179fffe
Victoria Kee <victoriakee95@gmail.com>
2021-12-02 12:53:21 -0600

    add errmess prop to comments in campsiteInfo

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -58,7 +58,8 @@ class Main extends Component {
                     campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     campsitesLoading={this.props.campsites.isLoading}
                     campsitesErrMess={this.props.campsites.errMess}
-                    comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}  
+                    comments={this.props.comments.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}  
+                    commentsErrMess={this.props.comments.errMess}
                     addComment={this.props.addComment}
                 />
             )
